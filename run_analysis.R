57bae17380374b84587d115c95328e169a4628dd
AxelSchwanke <axel.schwanke@gmx.de>
2014-04-17 16:48:50 +0200

    created

---
+++ run_analysis.R
@@ -0,0 +1,125 @@
+#
+# 1. Merges the training and the test sets to create one data set
+#
+# train data in ./train/X_train.txt
+# test data in ./test/X_test.txt
+#
+# merge data in ./data/X.txt
+#
+
+setwd("E://MyDocuments/UNIVERSITIES/COURSERA - Getting and Cleaning Data/peer assessment/UCI HAR Dataset")
+getwd()
+
+X1 <- read.table("./train/X_train.txt")
+X2 <- read.table("./test/X_test.txt")
+X <- rbind(X1, X2)
+
+dim(X1)
+dim(X2)
+dim(X)
+names(X)
+
+head(X1)
+summary(X1)
+str(X1)
+
+subject1 <- read.table("./train/subject_train.txt")
+subject2 <- read.table("./test/subject_test.txt")
+subject <- rbind(subject1, subject2)
+
+y1 <- read.table("./train/y_train.txt")
+y2 <- read.table("./test/y_test.txt")
+y <- rbind(y1, y2)
+dim(y)
+
+features <- read.table("./features.txt")
+dim(features)
+
+#
+# 4. Appropriately labels the data set with descriptive activity names.
+# or better
+# 4. Appropriately labels the data set with descriptive variable or feature (column) names
+#
+# TODO: better more descriptive names
+#
+#
+
+names(X) <- features[,2]
+names(y) <- "activity"
+names(subject)<- "subject"
+
+
+data <- cbind(subject,X,y)
+dim(data)
+names(data)
+
+#
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+#
+# remove all columns that NOT contain '-mean' or '-std', 
+# except column 1 (subject) and column 563 (activity)
+#
+
+rlist = vector()
+for (i in 2:(dim(data)[2]-1)) {
+  name = names(data)[i]
+  if ( grepl("-mean\\(",name) == FALSE & grepl("-std\\(",name) == FALSE ) {
+    rlist <- c(rlist,i)
+  }
+}
+data <- data[,-rlist]
+dim(data)
+
+#
+# 3. Uses descriptive activity names to name the activities in the data set
+#
+#               
+
+activityLabels <- read.table("./activity_labels.txt")
+data$activity <- activityLabels[data$activity,2]
+
+
+
+#
+# 4. Appropriately labels the data set with descriptive activity names.
+# or better
+# 4. Appropriately labels the data set with descriptive variable or feature (column) names
+#
+# TODO: better more descriptive names
+#
+#
+
+require(seqinr)
+
+# short-cut to sanitized column names: (coerce out `-` and `(` and `)`)
+data <- data.frame(data, check.rows=TRUE)
+
+names(data) <- gsub("\\.","_",names(data))
+names(data) <- trimSpace(names(data),space="_")
+names(data) <- gsub("(_)\\1+","_", names(data))
+
+names(data) <- gsub("_a","A", names(data))
+names(data) <- gsub("_b","B", names(data))
+names(data) <- gsub("_c","C", names(data))
+names(data) <- gsub("_e","E", names(data))
+names(data) <- gsub("_g","G", names(data))
+names(data) <- gsub("_i","I", names(data))
+names(data) <- gsub("_m","M", names(data))
+names(data) <- gsub("_s","S", names(data))
+names(data) <- gsub("_t","T", names(data))
+
+# 
+# 5. Create a second, independent tidy data set with 
+#    the average of each variable for each activity and each subject.
+#
+#
+
+require(reshape2)
+dataMelt <- melt(data, id=c("activity","subject"))
+dim(dataMelt)
+head(dataMelt,n=15)
+
+dataCast <- dcast(dataMelt, activity + subject ~ variable,mean)
+dim(dataCast)
+names(dataCast)
+head(dataCast[,1:4],n=50)
